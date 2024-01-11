619f3a074a8e76072d3aac9d264447c29c074f5b
Ersin Çöl <ersincol235@gmail.com>
2024-01-11 09:55:01 +0300

    Create searchbar component

--- src/components/TheRightbar.vue
+++ src/components/TheRightbar.vue
@@ -1,7 +1,9 @@
-<script setup></script>
+<script setup>
+import RightbarSearch from "@/components/RightbarSearch.vue";
+</script>
 <template>
     <aside class="w-[350px] mr-2.5">
-        RightBar
+        <RightbarSearch />
     </aside>
 </template>
 <style scoped></style>
