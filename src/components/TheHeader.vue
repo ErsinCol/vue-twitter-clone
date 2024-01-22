82ae6849993094f7cb3bb882b2a823370351d4c7
Ersin Çöl <ersincol235@gmail.com>
2024-01-22 18:10:27 +0300

    Create header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <header
+    class="sticky border-b border-[color:var(--background-third)] top-0 z-10 bg-[color:var(--background-primary-alpha)] backdrop-blur-md"
+  >
+    <h3 class="px-4 h-[3.313rem] flex items-center text-xl font-bold">
+      {{title}}
+    </h3>
+    <slot />
+  </header>
+</template>
+
+<style scoped>
+
+</style>
