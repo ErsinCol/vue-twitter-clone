e2bd7aef3cd6a25d063e3a8f6982f9874cef72d7
Ersin Çöl <ersincol235@gmail.com>
2024-01-22 17:54:09 +0300

    Create buttons to open specific tab content. Style the buttons and the tab content

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,26 @@
-<script setup></script>
+<script setup>
+
+function openTab(tabName){
+
+}
+
+</script>
 
 <template>
     <h2>Home Page</h2>
+    <!-- Tab Links -->
+    <div class="overflow-hidden border border-[#ccc] bg-[#f1f1f1]">
+      <button class="bg-inherit float-left border-none outline-0 cursor-pointer py-[14px] px-4 transition-colors hover:bg-[#ddd] active:bg-[#ccc]" @click="openTab('for-you')">For your</button>
+      <button class="bg-inherit float-left border-none outline-0 cursor-pointer py-[14px] px-4 transition-colors hover:bg-[#ddd] active:bg-[#ccc]" @click="openTab('following')">Following</button>
+    </div>
+    <!-- Tab Contents-->
+    <div id="for-you" class="hidden py-1.5 px-3 border-t-0">
+      <h3>For you section</h3>
+    </div>
+
+    <div id="following" class="hidden py-1.5 px-3 border-t-0">
+      <h3>Following section</h3>
+    </div>
 </template>
 
 <style></style>
