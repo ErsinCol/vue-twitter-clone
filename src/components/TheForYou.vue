547c31b7f0808929f16c325735fe83c83624cc01
Ersin Çöl <ersincol235@gmail.com>
2024-01-23 11:56:12 +0300

    Implement of its own Infinite Scroll Logic.Remove of virtua package

--- src/components/TheForYou.vue
+++ src/components/TheForYou.vue
@@ -1,21 +1,15 @@
 <script setup>
 import {ref, onMounted, onUnmounted} from "vue";
-import { VList } from "virtua/vue";
 import BasePost from "@/components/BasePost.vue";
 import {posts} from "@/mock/posts.js";
 
 const allPost = ref(posts)
-let nextPostId = allPost.value.length + 1;
 
-const handleScroll = () => {
-  const scrollY = window.scrollY;
-  const windowHeight = window.innerHeight;
-  const documentHeight = document.documentElement.scrollHeight;
+function handleScroll(){
+  let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight >= document.documentElement.offsetHeight - 1;
 
-  if (scrollY + windowHeight >= documentHeight - 200) {
-    allPost.value = allPost.value.concat(
-        posts.map((post) => ({ ...post, id: nextPostId++ }))
-    );
+  if(bottomOfWindow){
+    allPost.value = allPost.value.concat(posts)
   }
 }
 
@@ -29,14 +23,11 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <VList :data="allPost" class="custom-virtual-list">
-    <template #default="post">
-      <BasePost
-          :key="post.id"
-          :post="post"
-      />
-    </template>
-  </VList>
+    <BasePost
+        v-for="(post, index) in allPost"
+        :key="index"
+        :post="post"
+    />
 </template>
 
 <style scoped>
