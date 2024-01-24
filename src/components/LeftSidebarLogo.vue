e2ed205eae4424ee9183dbc152abcb5bd5c1d62a
Ersin Çöl <ersincol235@gmail.com>
2024-01-24 23:42:24 +0300

    Fix size the logo

--- src/components/LeftSidebarLogo.vue
+++ src/components/LeftSidebarLogo.vue
@@ -4,7 +4,7 @@
 <template>
     <div class="py-0.5">
         <RouterLink :to="{ name: 'Home' }" class="text-[color:var(--color-base)] w-[3.25rem] h-[3.25rem] rounded-full flex items-center justify-center hover:bg-[color:var(--background-secondary)] transition-colors">
-            <BaseIcon name="logo" />
+            <BaseIcon name="logo" class="w-[1.875rem] h-[1.875rem]"/>
         </RouterLink>
     </div>
 </template>
