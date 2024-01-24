fd1f63dd9978da064188027b1ec2c93d60717b44
Ersin Çöl <ersincol235@gmail.com>
2024-01-24 17:01:24 +0300

    Implementing list virtualization with vue-virtual-scroller library

--- src/components/TheForYou.vue
+++ src/components/TheForYou.vue
@@ -1,16 +1,33 @@
 <script setup>
-import {ref, onMounted, onUnmounted} from "vue";
+import {ref, onMounted, onUnmounted, computed} from "vue";
 import BasePost from "@/components/BasePost.vue";
 import {posts} from "@/mock/posts.js";
 
-const allPost = ref(posts)
+const allPost = ref([])
 
-function handleScroll(){
+onMounted(()=>{
+  allPost.value = posts
+})
+
+function handleScroll() {
   let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight >= document.documentElement.offsetHeight - 1;
 
-  if(bottomOfWindow){
-    allPost.value = allPost.value.concat(posts)
+  if (bottomOfWindow) {
+    addMorePost()
   }
+}
+
+
+function addMorePost(){
+  const updatedPosts = posts.map((post) => {
+    return {
+      ...post,
+      id: (parseInt(post.id) + allPost.value.length).toString(),
+    };
+  });
+
+  // Append the updated posts to the existing ones
+  allPost.value = [...allPost.value, ...updatedPosts];
 }
 
 onMounted(()=>{
@@ -23,13 +40,25 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <BasePost
-        v-for="(post, index) in allPost"
-        :key="index"
-        :post="post"
-    />
+  <DynamicScroller
+      :items="allPost"
+      :min-item-size="1"
+      class="scroller"
+  >
+    <template v-slot="{ item, index, active }">
+      <DynamicScrollerItem
+          :item="item"
+          :active="active"
+          :data-index="index"
+      >
+        <BasePost :post="item" :key="item.id"></BasePost>
+      </DynamicScrollerItem>
+    </template>
+  </DynamicScroller>
 </template>
 
-<style scoped>
-
+<style>
+.scroller {
+  height: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import "@/style.css"
+import {DynamicScroller, DynamicScrollerItem} from "vue-virtual-scroller";
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 import App from '@/App.vue'
 import router from '@/routes/index.js'
 import { createPinia } from 'pinia'
 
 const pinia = createPinia()
 const app = createApp(App)
+
+app.component('DynamicScroller', DynamicScroller)
+app.component('DynamicScrollerItem', DynamicScrollerItem)
 
 app.use(pinia)
 app.use(router)
