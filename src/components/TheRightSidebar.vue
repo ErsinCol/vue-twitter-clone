799040fe086b4af67157ba3cd3d3a442ac58f2ea
Ersin Çöl <ersincol235@gmail.com>
2024-01-26 15:56:01 +0300

    Implement twitter like sticky sidebar

--- src/components/TheRightSidebar.vue
+++ src/components/TheRightSidebar.vue
@@ -9,23 +9,79 @@ import {onMounted, onUnmounted, ref} from "vue";
 const stickySidebarEl = ref(null);
 const position = ref('relative')
 const top = ref(0)
-let lastScrollTop = 0;
+let prevScrollTop = window.scrollY;
+let topPositionAtWhichElementBecomesStickyFromTop = 0
+let offset;
 
-function scrollHandler(){
-  // scroll direction'u tespit etmek için önceki scroll top değerini saklayabilir ve anlık olan scroll top ile karşılaştırabilirsin.
-  // Element.scrollTop, bir elementin içeriğinin dikey olarak kaydırıldığı pixel sayısı, öğenin üst kısmından en üstteki görünen içeriğine olan mesafe
-  console.log(document.documentElement.scrollTop)
-  let st = document.documentElement.scrollTop;
-  if(st > lastScrollTop){
-    console.log("aşağı kaydırıyorum.")
-  }else if(st < lastScrollTop){
-    console.log("yukarı kaydırıyorum.")
+const scrollHandler = () => {
+  const elementBoundingRect = stickySidebarEl.value.getBoundingClientRect();
+  const elementBoundingRectTop = Math.ceil(elementBoundingRect.top);
+  const elementBoundingRectBottom = Math.ceil(elementBoundingRect.bottom);
+  const topDistanceOfElementRelativeToPageTop = Math.ceil(stickySidebarEl.value.offsetTop);
+  const topDistanceAtWhichElementBecomesStickyFromBottom = elementBoundingRectTop;
+
+  const scrollYOffset = window.scrollY;
+  const isScrollingUp = scrollYOffset < prevScrollTop;
+  const isScrollingDown = scrollYOffset > prevScrollTop;
+  const windowHeight = document.documentElement.clientHeight;
+
+  const topEndPosition = elementBoundingRectTop - topPositionAtWhichElementBecomesStickyFromTop
+  const bottomEndPosition = elementBoundingRectBottom;
+
+  const isTopEndAboveViewport = topEndPosition < 0;
+  const isTopEndBelowViewport = topEndPosition > windowHeight;
+  const isBottomEndBelowViewport = bottomEndPosition > windowHeight;
+  const isBottomEndAboveViewport = bottomEndPosition < 0;
+
+  const isTopEndBetweenViewport = !isTopEndAboveViewport && !isTopEndBelowViewport;
+  const isBottomEndBetweenViewport = !isBottomEndAboveViewport && !isBottomEndBelowViewport;
+
+  const areBothTopAndBottomEndsOnOppositeEndsOfViewport = isTopEndAboveViewport && isBottomEndBelowViewport;
+  const areBothTopAndBottomEndsBetweenViewport = isTopEndBetweenViewport && isBottomEndBetweenViewport;
+
+  if (isTopEndBelowViewport || isBottomEndAboveViewport) {
+    position.value = 'relative';
+    top.value = scrollYOffset
+    prevScrollTop = scrollYOffset;
+    return;
   }
-  lastScrollTop = st <= 0 ? 0 : st;
-}
+  if (areBothTopAndBottomEndsOnOppositeEndsOfViewport) {
+    position.value = 'relative'
+    top.value = topDistanceOfElementRelativeToPageTop - offset;
+    prevScrollTop = scrollYOffset;
+    return;
+  }
+
+  if (areBothTopAndBottomEndsBetweenViewport) {
+    position.value = 'sticky'
+    top.value = topPositionAtWhichElementBecomesStickyFromTop
+    prevScrollTop = scrollYOffset;
+    return;
+  }
+
+  if (isScrollingUp) {
+    if (isTopEndBetweenViewport) {
+      position.value = 'sticky'
+      top.value = topPositionAtWhichElementBecomesStickyFromTop
+    } else if (isBottomEndBetweenViewport) {
+      position.value = 'relative'
+      top.value = topDistanceOfElementRelativeToPageTop - offset
+    }
+  } else if (isScrollingDown) {
+    if (isTopEndBetweenViewport) {
+      position.value = 'relative'
+      top.value = topDistanceOfElementRelativeToPageTop - offset
+    } else if (isBottomEndBetweenViewport) {
+      position.value = 'sticky'
+      top.value = topDistanceAtWhichElementBecomesStickyFromBottom
+    }
+  }
+  prevScrollTop = scrollYOffset;
+};
 
 onMounted(()=>{
   window.addEventListener('scroll', scrollHandler);
+  offset = Math.ceil(stickySidebarEl.value.offsetTop);
 })
 
 onUnmounted(()=>{
@@ -35,7 +91,7 @@ onUnmounted(()=>{
 <template>
   <aside
       ref="stickySidebarEl"
-      class="w-[350px]"
+      class="w-[350px] self-start"
       :style="{
         top: `${top}px`,
         position: position,
